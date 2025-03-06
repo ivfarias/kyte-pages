d51cb8bf26faa00c80d01505523a921de0ec79e3
ivfarias <imivanfarias@gmail.com>
2025-03-06 13:23:25 -0300

    renamed:    src/api/kyteAuth.js -> public/api/kyteAuth.js 	modified:   src/components/ui/EmailSubmit.tsx

--- src/components/ui/EmailSubmit.tsx
+++ src/components/ui/EmailSubmit.tsx
@@ -42,7 +42,7 @@ export const EmailSubmit = ({
     useEffect(() => {
         // Load the external script
         const script = document.createElement("script");
-        script.src = "./src/api/kyteAuth.js";
+        script.src = "/public/api/kyteAuth.js";
         script.async = true;
         document.body.appendChild(script);
 
